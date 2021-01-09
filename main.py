2a1c96410b74c4ff9d20d63a0df7a35c30b55cb8
Gabriel Christo <christo@dcc.ufrj.br>
2021-01-09 01:53:25 -0200

    added stylesheet file

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from PyQt5.QtWidgets import QApplication
 
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
+	with open("style.css", "r") as style: app.setStyleSheet(style.read())
 	c2 = Convert2()
 	c2.show()
 	sys.exit(app.exec_())
